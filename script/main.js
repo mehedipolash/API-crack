fe5dd6b1e447084b5a111806f0ab09f2fff9ec39
mehedipolash <mehedipolash228@gmail.com>
2025-04-17 00:50:47 +0600

    dynamically added video thumbnail card from api

--- script/main.js
+++ script/main.js
@@ -1,3 +1,7 @@
+/* ---------------------------------------------------------------------------- */
+
+//load categorybuttons dynamically and display page
+
 function loadCategory() {
   // 1 - fetch the data
   fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
@@ -19,8 +23,7 @@ function displayCategory(receivedCategories) {
   const categoryContainer = document.getElementById('category-container');
   //loop operation on array of object
   for (let cat of receivedCategories) {
-    console.log(cat);
-
+    
     //create element
     const categoryDiv = document.createElement('div');
     categoryDiv.innerHTML = `
@@ -30,3 +33,54 @@ function displayCategory(receivedCategories) {
    categoryContainer.append(categoryDiv)
   }
 }
+/* ---------------------------------------------------------------------------- */
+
+
+
+
+/* -------------------------------------------------------------------------------- */
+
+function loadVideos() {
+  fetch('https://openapi.programming-hero.com/api/phero-tube/videos')
+    .then(res => res.json())
+  .then(data=>displayVideos(data.videos))
+}
+
+loadVideos();
+   
+function displayVideos(videos) {
+   const videoContainer=document.getElementById('video-container')
+  for (video of videos) {
+   
+    
+    const videoCard = document.createElement('div');
+    videoCard.innerHTML = `
+     <div class="shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.title}</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div class="card-actions justify-end">
+      <button class="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
+    
+    
+    `;
+    videoContainer.append(videoCard);
+    }
+  }
+
+/* -------------------------------------------------------------------------------- */
+
+
+
+
+
+
+
+
